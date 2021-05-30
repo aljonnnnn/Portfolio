3136dd24a4999177cca353408d70fa5ce5c61df3
aljonnnnn <delumenaljon@gmail.com>
2021-05-30 23:48:08 +0800

    Install styled-components

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -9,50 +9,50 @@ const AboutMe = () => {
 
     return (
         <div className="about" id="about">
-                <div className="container">
-                    <h2 className="section-heading">{aboutMeData.title}</h2>
-                    <div className="about__flex">
-                        <div className="about__box">
-                            <div className="about__item text-center">
-                                <img src={aboutMeData.img} alt="" className="about__img"/>
-                            </div>
+            <div className="container">
+                <h2 className="section-heading">{aboutMeData.title}</h2>
+                <div className="about__flex">
+                    <div className="about__box">
+                        <div className="about__item text-center">
+                            <img src={aboutMeData.img} alt="" className="about__img"/>
                         </div>
-                        <div className="about__box">
-                            <div className="about__item">
-                                <h3 className="about__heading">{aboutMeData.heading}</h3>
-        
-                                {aboutMeData.paragraphs.map((paragraph, index) => {
-                                    return (
-                                        <p className="about__paragraph" key={index}>{paragraph}</p>
-                                    )
-                                })}
+                    </div>
+                    <div className="about__box">
+                        <div className="about__item">
+                            <h3 className="about__heading">{aboutMeData.heading}</h3>
+    
+                            {aboutMeData.paragraphs.map((paragraph, index) => {
+                                return (
+                                    <p className="about__paragraph" key={index}>{paragraph}</p>
+                                )
+                            })}
 
-                                {/* <Button 
-                                    href='resume.pdf' 
-                                    className='btn btn-primary' 
-                                    text='resume'
-                                    clicked={useScrollTop}
-                                />     */}
+                            {/* <Button 
+                                href='resume.pdf' 
+                                className='btn btn-primary' 
+                                text='resume'
+                                clicked={useScrollTop}
+                            />     */}
 
-                                <a 
-                                    href={resume} 
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className='btn btn-primary'>
-                                    resume
-                                </a>
+                            <a 
+                                href={resume} 
+                                target="_blank"
+                                rel="noreferrer"
+                                className='btn btn-primary'>
+                                resume
+                            </a>
 
-                                <Button 
-                                    href='/portfolio' 
-                                    className='btn btn-secondary' 
-                                    text='portfolio'
-                                    clicked={useScrollTop}
-                                />
-                            </div>
+                            <Button 
+                                href='/portfolio' 
+                                className='btn btn-secondary' 
+                                text='portfolio'
+                                clicked={useScrollTop}
+                            />
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
